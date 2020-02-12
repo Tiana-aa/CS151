17374f76e1a23500a10974f1b1132413bf31bb88
Tiana-aa <tianafrancis@mec.science>
2020-02-11 20:39:36 -0500

    feb11

---
+++ labs/lab01.cpp
@@ -0,0 +1,48 @@
+/*
+  Date: Feb 11
+  Assignment: In-class Lab01
+  Name: <Your First and Last Name> Tiana Francis
+
+  To compile: g++ -o lab01 lab01.cpp
+  To execute: ./lab01
+
+*/
+
+#include<iostream>
+
+using namespace std;
+
+int main()
+{
+    // Todo:
+    // Task-0
+    // <type> <indentifier>;
+    int age;
+    string firstName;
+    string lastName; 
+    string firstName, lastName;
+
+    // Task-1
+    firstName = "Ariana";
+    lastName = "Grande";
+    age = 26;
+
+    // Task-2
+    cout << firstName << "\n" << lastName << "\n" <<age << "\n";
+
+        // cout <<"\n\n\n";
+
+    // Task-3    
+    cout <<&firstName << "|" <<&lastName << "|" <<&age << "\n"
+
+    // Task-4
+
+	
+
+    return 0;
+
+} // end of the main function
+
+
+
+
