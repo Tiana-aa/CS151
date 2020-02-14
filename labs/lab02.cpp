642014909377f222cb6253eb8092b079bc2a38d0
Tiana-aa <tianafrancis@mec.science>
2020-02-13 20:20:52 -0500

    Lab02 Completed

---
+++ labs/lab02.cpp
@@ -0,0 +1,57 @@
+/*
+  Date: Feb 13
+  Assignment: In-class Lab02
+  Name: Tiana Francis
+
+  To compile: g++ -o lab02 lab02.cpp
+  To execute: ./lab02
+
+*/
+#include <iostream>
+using namespace std;
+
+int main()
+{    
+
+    // Task 0 Image
+    cout << "***************\n";
+    cout << "***************\n";
+    cout << "***************\n";
+    cout << "***************\n";
+    cout << "***************\n";
+    cout << "***************\n";
+    // todo
+
+
+
+
+    // Task 1 Image
+    cout << "\n******    ******\n";
+    cout << "******    ******\n";
+    cout << "  **        **  \n";
+    cout << "  **        **  \n";
+    cout << "  **        **  \n";
+    cout << "  **        **  \n";
+    cout << "******      **  \n";
+    cout << "******      **  \n";
+    // todo
+
+
+
+
+    // Task 2 Image
+    cout << " \n                **\n";
+    cout << "               ****\n";
+    cout << "              ******\n";
+    cout << "             ********\n";
+    cout << "            **********\n";
+    cout << "           ************\n";
+    cout << "          **************\n";
+    cout << "         ****************\n";
+    
+    // todo
+
+
+
+    return 0;
+}
