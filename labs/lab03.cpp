4f3bd6cfeffa991151adcf81d08dd836d15ac34a
Tiana-aa <tianafrancis@mec.science>
2020-02-20 20:15:48 -0500

    lab04 completed

---
+++ labs/lab03.cpp
@@ -0,0 +1,39 @@
+/*
+  Date: Feb 13
+  Assignment: In-class Lab03
+  Name: Tiana Francis
+
+  To compile: g++ -o lab03 lab03.cpp
+  To execute: ./lab03
+
+*/
+
+#include <iostream>
+using namespace std;
+
+int main()
+{    
+    string a;
+    string b;
+    string a = "Hello";
+    string b = "World!"
+
+    cout << "Hello, Hello World!\n"; 
+
+    // Todo
+    
+
+
+    cout << "\nTask 1:\n";
+    // Todo
+
+
+
+    cout << "\nTask 2: \n";
+    // Todo
+
+
+
+
+    return 0;
+}
---
+++ labs/lab04.cpp
@@ -0,0 +1,23 @@
+/* 
+Date February 20, 2020
+Assignment: In class lab04
+Tiana Francis
+
+*/
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    string fname = "Tiana";
+    string lname = "Francis,";
+    
+    cout << "*************\n";
+    cout << lname  << fname << "\n";
+    cout << "*************\n";
+    
+
+    return 0;
+}
+
